49495f8dcf3be57e918ed68e44254c860ed9a2bf
odilonneto <netoodilon175@gmail.com>
2025-01-24 15:56:22 -0300

    Home Page

--- app/src/main/java/com/example/pm26sproject2/HomeActivity.kt
+++ app/src/main/java/com/example/pm26sproject2/HomeActivity.kt
@@ -2,16 +2,27 @@ package com.example.pm26sproject2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
 
+    private var isMenuVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnMenu: Button = findViewById(R.id.btnMenu)
+        val sideMenu: LinearLayout = findViewById(R.id.sideMenu)
+
+        btnMenu.setOnClickListener {
+            toggleMenu(sideMenu)
+        }
 
         val btnLogout: Button = findViewById(R.id.btnLogout)
         btnLogout.setOnClickListener {
@@ -20,5 +31,24 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+    }
+    private fun toggleMenu(menu: View) {
+        if (isMenuVisible) {
+            menu.animate()
+                .translationX(menu.width.toFloat())
+                .setDuration(300)
+                .withEndAction {
+                    menu.visibility = View.GONE
+                }
+                .start()
+        } else {
+            menu.visibility = View.VISIBLE
+            menu.animate()
+                .translationX(600f)
+                .setDuration(300)
+                .start()
+        }
+        isMenuVisible = !isMenuVisible
     }
 }
