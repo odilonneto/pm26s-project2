721f2282ea641b3f122227f0b3b3a950fff5202c
Victor <47982205+VictorCFerreira@users.noreply.github.com>
2025-02-23 14:05:22 -0300

    Group adapter

--- app/src/main/java/com/example/pm26sproject2/Adapter/GroupAdapter.kt
+++ app/src/main/java/com/example/pm26sproject2/Adapter/GroupAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.pm26sproject2.Adapter
 
-class GroupAdapter {
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pm26sproject2.R
+import com.example.pm26sproject2.entity.Group
+
+class GroupAdapter(
+    private val groups: List<Group>,
+    private val onItemClick: (Group) -> Unit
+) : RecyclerView.Adapter<GroupAdapter.GroupViewHolder>() {
+
+    inner class GroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(group: Group) {
+
+            val name = group.groupName
+            val description = group.groupDescription
+
+            itemView.findViewById<TextView>(R.id.tvName).text = name
+            itemView.findViewById<TextView>(R.id.tvDescription).text = description
+
+            itemView.setOnClickListener {
+                onItemClick(group)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.dialog_exercise, parent, false)
+        return GroupViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GroupViewHolder, position: Int) {
+        holder.bind(groups[position])
+    }
+
+    override fun getItemCount(): Int = groups.size
 }
